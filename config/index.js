fbcd4cb62dbaa3e932458c0b03f49ac5e4837a60
tangdaoyuan <1197633750@qq.com>
2019-07-16 10:49:41 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -12,11 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        // target: 'http://101.236.19.193:16800',
-        // target: 'http://101.236.57.220:16800/',
-        // target: 'http://localhost:8080',
-        target:'http://43.241.237.154:8081',
-        // target:'http://192.168.0.46:8081',
+        target: 'ip addr'
         changeOrigin: true
       }
     },
